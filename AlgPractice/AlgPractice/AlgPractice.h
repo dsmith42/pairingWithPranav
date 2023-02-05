8aef106455f87784b50e23a4e28f99a17aa3f12a
Dan Smith <73941747+dsmith42@users.noreply.github.com>
2023-02-05 15:35:24 +0000

    Starting point

---
+++ AlgPractice/AlgPractice/AlgPractice.h
@@ -0,0 +1,18 @@
+//
+//  AlgPractice.h
+//  AlgPractice
+//
+//  Created by Dan Smith on 05/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AlgPractice.
+FOUNDATION_EXPORT double AlgPracticeVersionNumber;
+
+//! Project version string for AlgPractice.
+FOUNDATION_EXPORT const unsigned char AlgPracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlgPractice/PublicHeader.h>
+
+
---
+++ AlgPractice/AlgPracticeTests/C057IsoMorphicValuesTests.swift
@@ -0,0 +1,16 @@
+//
+//  C057IsoMorphicValuesTests.swift
+//  AlgPracticeTests
+//
+//  Created by Dan Smith on 05/02/2023.
+//
+
+import XCTest
+@testable import AlgPractice
+
+final class C057IsoMorphicValuesTests: XCTestCase {
+
+	func test_zero() {
+	}
+
+}
